55c4aef28f5c375b76aa9066c7159c47ad80464d
Georgiy Lukjanov <georgiylukjanov@gmail.com>
2017-02-24 12:28:52 +0300

    Refine Main.hs
    Make type of example program more abstract
    Add example for Listing

--- Main.hs
+++ Main.hs
@@ -1,25 +1,43 @@
 import Microprogram
-import Simulation
+import qualified Simulation as S
+import qualified Listing as L
 
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as Map
 
-program :: Simulation ()
-program = do
-	writeMemory 100 200
-	writeRegister (R 0) 1234
+program :: Microprogram m => Register m -> Register m -> m ()
+program reg0 reg1 = do
+    writeMemory 100 200
+    writeMemory 100 200
+    writeRegister reg0 1234
 
-	let programAddress = 10000
-	writeRegister pc programAddress
-	writeMemory (programAddress + 1) 100
-	writeMemory (programAddress + 2) 222
+    let programAddress = 10000
+    writeRegister pc programAddress
+    writeMemory (programAddress + 1) 100
+    writeMemory (programAddress + 2) 222
 
-	load (R 1)
+    load reg1
 
-processor = Processor (Map.empty) (Map.empty)
+programSimulation :: S.Simulation ()
+programSimulation =
+    program (S.R 0) (S.R 1)
+
+programListing :: L.Listing ()
+programListing =
+    program (L.Register "0") (L.Register "1")
+
+processor = S.Processor (Map.empty) (Map.empty)
 
 main = do
-	putStrLn $ "\nInitial processor:\n" ++ show processor ++ "\n"
-	let (value, newProcessor) = simulate program processor
-	putStrLn $ "Value = " ++ show value ++ "\n"
-	putStrLn $ "New processor:\n" ++ show newProcessor ++ "\n"
+    putStrLn "==============="
+    putStrLn "=== Listing ==="
+    putStrLn "==============="
+    putStrLn $ L.listing programListing
+
+    putStrLn "=================="
+    putStrLn "=== Simulation ==="
+    putStrLn "=================="
+    putStrLn $ "\nInitial processor:\n" ++ show processor ++ "\n"
+    let (value, newProcessor) = S.simulate programSimulation processor
+    putStrLn $ "Value = " ++ show value ++ "\n"
+    putStrLn $ "New processor:\n" ++ show newProcessor ++ "\n"
